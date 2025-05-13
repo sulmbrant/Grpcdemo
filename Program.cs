1f367d3ddb2474d207dca9b9de75fa3f0f17de61
sulmbrant <53706622+sulmbrant@users.noreply.github.com>
2025-05-13 09:52:24 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,8 @@
-using Grpc.Net.Client;
 using GrpcChatDemo;
 
-var channel = GrpcChannel.ForAddress("https://localhost:5001");
-var client = new Chat.ChatClient(channel);
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpc();
 
-using var chat = client.ChatStream();
-
-var readTask = Task.Run(async () =>
-{
-    await foreach (var message in chat.ResponseStream.ReadAllAsync())
-    {
-        Console.WriteLine($"Server: {message.Text}");
-    }
-});
-
-for (int i = 0; i < 5; i++)
-{
-    await chat.RequestStream.WriteAsync(new ChatMessage { Text = $"Hej {i}" });
-    await Task.Delay(500);
-}
-
-await chat.RequestStream.CompleteAsync();
-await readTask;
+var app = builder.Build();
+app.MapGrpcService<ChatService>();
+app.Run();
